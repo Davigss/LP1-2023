1f20117a0c1c04ccf469e89c9ab5a5d2d803f63b
Davigss <126773080+Davigss@users.noreply.github.com>
2023-05-11 09:11:15 -0300

    Update Main.java

--- 2°Bimestre/Biblioteca/src/biblioteca/Main.java
+++ 2°Bimestre/Biblioteca/src/biblioteca/Main.java
@@ -6,26 +6,16 @@ import usuarios.Livro;
 
 public class Main {
 	public static void main(String[] args) {
-		Biblioteca biblioteca = new Biblioteca("Livraria Lira", new Dono("Fernando Freitas", 34425780990L, 9845434783L));
-
-		Funcionario f1 = new Funcionario("Danilo Teixeira", 147194593915L, 295810583L, "Bibliotecario chefe");
-
-		Funcionario f2 = new Funcionario("Melissa Cardoso", 12958693953L, 1294024059L, "Faxineira");
-
-		Livro l1 = new Livro("Admirável Mundo novo", "Aldous Huxley", 1932);
-
-		Livro l2 = new Livro("As memórias póstumas de Brás Cubas", "Machado de Assis", 1881);
-
-		Livro l3 = new Livro("A revolução dos bichos", "George Orwell", 1928);
+		Biblioteca biblioteca = new Biblioteca("Livraria Leitura", new Dono("Davi Gomes", 47598780990L, 9848754783L));
 
 		System.out.printf("Biblioteca '%s'\n", biblioteca.getNome());
 		System.out.printf("Dono atual: '%s'\n\n", biblioteca.getDono());
 
-		biblioteca.addFuncionario(f1);
-		biblioteca.addFuncionario(f2);
-		biblioteca.addLivros(l1);
-		biblioteca.addLivros(l2);
-		biblioteca.addLivros(l3);
+		biblioteca.addFuncionario(new Funcionario("Mister  Rocha", 147194593915L, 295810583L, "Bibliotecario chefe"));
+		biblioteca.addFuncionario(new Funcionario("Cucorucho", 12958693953L, 1294024059L, "Faxineira"));
+		biblioteca.addLivros(new Livro("Herobrine: A Lenda", "Tazercraft", 2016));
+		biblioteca.addLivros(new Livro("O Grande Irmão", "George Orwell", 1984));
+		biblioteca.addLivros(new Livro("Deuses de Neon", "Karee Robert", 2021));
 		biblioteca.mostraFuncionario();
 		biblioteca.mostraLivros();
 
